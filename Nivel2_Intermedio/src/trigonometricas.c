c6a0aa558588c3edb6f20d493c55cb0aceb8b977
Moisés Rojas <rojasmata.moy@gmail.com>
2026-03-01 23:55:06 -0600

    Definición de estructura

--- Nivel2_Intermedio/src/trigonometricas.c
+++ Nivel2_Intermedio/src/trigonometricas.c
@@ -1,3 +1,87 @@
 //
 // Created by Moisés Rojas on 01/03/2026.
 //
+
+// Implementacion Nivel 2 (Reduccion de argumento y estabilizacion)
+
+#include <stdio.h>
+#include <math.h>
+#include "../include/series_v2.h" // Vinculación con el contrato
+
+#define PI 3.14159265358979323846
+
+// JP: Funciones Trigonometricas Base (19 - 22)
+
+double serie_19_seno_v2(double x, int n) {
+    // TODO JP: Reducir x usando fmod(x, 2.0 * PI)
+    // TODO JP: Insertar motor de sumatoria
+    return 0.0;
+}
+
+double serie_20_coseno_v2(double x, int n) {
+    // TODO JP: Reducir x usando fmod(x, 2.0 * PI)
+    return 0.0;
+}
+
+double serie_21_tangente_v2(double x, int n) {
+    // TODO JP: Tangente tiene asíntotas. Validar que x no sea múltiplo de PI/2.
+    return 0.0;
+}
+
+double serie_22_secante_v2(double x, int n) {
+    // TODO JP: Validar dominio similar a la tangente.
+    return 0.0;
+}
+
+// MOY: Inversas, Hiperbolicas y Varias (23 - 33)
+
+double serie_23_cosecante_v2(double x, int n) {
+    // TODO Moy: Validar que x no sea múltiplo de PI (divisiones entre cero).
+    return 0.0;
+}
+
+double serie_24_arcsin_v2(double x, int n) {
+    // TODO Moy: Validar dominio matemático: -1 <= x <= 1
+    return 0.0;
+}
+
+double serie_25_arccos_v2(double x, int n) {
+    // TODO Moy: Validar dominio matemático: -1 <= x <= 1
+    return 0.0;
+}
+
+double serie_26_arctan_v2(double x, int n) {
+    // TODO Moy: Validar dominio matemático: -1 <= x <= 1
+    return 0.0;
+}
+
+double serie_27_senh_v2(double x, int n) {
+    // TODO Moy: Funcion no periodica (crece muy rápido).
+    // La protección viene de limitar 'n' y 'x' en el main.
+    return 0.0;
+}
+
+double serie_28_cosh_v2(double x, int n) {
+    return 0.0;
+}
+
+double serie_29_tanh_v2(double x, int n) {
+    return 0.0;
+}
+
+double serie_30_csenh_v2(double x, int n) {
+    return 0.0;
+}
+
+double serie_31_arctanh_v2(double x, int n) {
+    // TODO Moy: Validar dominio estricto: -1 < x < 1
+    return 0.0;
+}
+
+double serie_32_serie_combinada_v2(double x, int n) {
+    return 0.0;
+}
+
+double serie_33_exp_seno_v2(double x, int n) {
+    return 0.0;
+}
