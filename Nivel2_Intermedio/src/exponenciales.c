51ce44715d9d7ab857b432536b8e56f5fb714934
Moisés Rojas <rojasmata.moy@gmail.com>
2026-03-01 23:57:35 -0600

    Estructura

--- Nivel2_Intermedio/src/exponenciales.c
+++ Nivel2_Intermedio/src/exponenciales.c
@@ -1,3 +1,48 @@
 //
 // Created by Moisés Rojas on 01/03/2026.
 //
+
+// Implementacion Nivel 2 (Validacion de Dominios y Overflow)
+
+#include <stdio.h>
+#include <math.h>
+#include "../include/series_v2.h" // Vinculación con el contrato
+
+// QUIM: Exponenciales y Logaritmos Iniciales (07 - 11)
+
+double serie_07_exp_v2(double x, int n) {
+    // TODO Quim: e^x crece de manera absurdamente rapida.
+    return 0.0;
+}
+
+double serie_08_x_exp_v2(double x, int n) {
+    // TODO Quim: Misma advertencia de desbordamiento (overflow) que la 07.
+    return 0.0;
+}
+
+double serie_09_x_x2_exp_v2(double x, int n) {
+    // TODO Quim: Misma advertencia de desbordamiento que la 07.
+    return 0.0;
+}
+
+double serie_10_ln1plusx_v2(double x, int n) {
+    // TODO Quim: GUARDIA DE DOMINIO REQUERIDA.
+    return 0.0;
+}
+
+double serie_11_variante_log_v2(double x, int n) {
+    // TODO Quim: GUARDIA DE DOMINIO REQUERIDA.
+    return 0.0;
+}
+
+// JP: Variantes de Logaritmo de x (12 - 13)
+
+double serie_12_ln_x_v1_v2(double x, int n) {
+    // TODO JP: GUARDIA DE DOMINIO REQUERIDA.
+    return 0.0;
+}
+
+double serie_13_ln_x_v2_v2(double x, int n) {
+    // TODO JP: GUARDIA DE DOMINIO REQUERIDA.
+    return 0.0;
+}
